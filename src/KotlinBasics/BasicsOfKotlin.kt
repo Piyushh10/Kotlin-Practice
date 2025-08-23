fac2e6fa56a77b35629a38eeff129ba2ed792b07
Piyushh10 <shivnanip815@gmail.com>
2025-08-24 00:40:26 +0530

    New project

---
+++ src/KotlinBasics/BasicsOfKotlin.kt
@@ -0,0 +1,10 @@
+package KotlinBasics
+
+fun main(){
+    var a : String
+    lateinit var b : String
+    /*
+    lateinit means late initialization, means we can initialize it late
+    this only works with var and not val due to obvious reasons
+     */
+}
