4c600c3f3060d81477a46d5d4c7a0e48cabeaee3
Piyushh10 <shivnanip815@gmail.com>
2025-08-24 17:20:10 +0530

    types of functions

--- src/Functions/FunctionsInKotlin.kt
+++ src/Functions/FunctionsInKotlin.kt
@@ -4,6 +4,13 @@ fun main(){
     greet()
     greet("Piyush")
     println(greetUsingReturn("Piyush"))
+    printData(10)
+    printData("Kya kaam hai?")
+    printData(1.69)
+    println(greetPaaji("Piyushinder", 20))
+    println(greetKar("Piyush Shivnani"){name-> "Hello ajeeb function waale $name"})
+
+
 }
 
 fun greet(){
@@ -15,3 +22,38 @@ fun greet(name: String){
 fun greetUsingReturn(name: String): String{
     return "Hello $name! (using return)"
 }
+fun greetUsingUnit():Unit{
+    println("Hello user! I'm using Unit return")
+}
+//unit is equivalent to void, but is actually a type unlike void which
+// is a keyword
+
+
+//single expression function
+fun square(n: Int) = n*n
+fun greeting() = println("Hello, greetings to you!")
+
+//generic function - it is a function that can work with any type of data
+fun <T> printData(data: T){
+    println("Data: $data")
+}
+
+
+//lambda function- a function without a name that can be assigned to a variable
+val greetPaaji : (String, Int) -> String = {name, age->
+    "Hello $name paaji, tussi $age saal ke hunde si?"
+}
+
+
+//higher order function- a function that takes another function as a
+//parameter or returns one
+fun greetKar(name: String, anotherFunc: (String)->String): String{
+    return anotherFunc(name)
+}
+
+
+
+
+
+
+
