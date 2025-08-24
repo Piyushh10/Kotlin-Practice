b8b0a2f2b55e590b00c1f6c7d6586f90cef4412d
Piyushh10 <shivnanip815@gmail.com>
2025-08-24 21:15:52 +0530

    interface

--- src/Interfaces/uses.kt
+++ src/Interfaces/uses.kt
@@ -1,2 +1,36 @@
 package Interfaces
 
+interface Engine{
+    fun start(){
+        println("Vehicle starts.....")
+    }
+    fun name(){
+        println("Engine")
+    }
+}
+
+interface Wheels{
+    fun roll(){
+        println("Wheels are rolling")
+    }
+    fun name(){
+        println("Wheels")
+    }
+}
+
+class car: Engine, Wheels{
+    override fun name(){
+        super<Wheels>.name()
+    }
+}
+
+fun main(){
+    val myCar = car()
+    myCar.start()
+    myCar.roll()
+}
+
+//conflicts- if 2 interfaces have same method, then we have to
+//explicitly override it in the implementation class to avoid conflict
+// for that we use super keyword
+//see name() in the above example
