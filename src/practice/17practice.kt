e077da52e2690c57ebce038b110ff44714b4e0f5
Piyushh10 <shivnanip815@gmail.com>
2025-10-05 11:45:10 +0530

    flow cancellation using withTimeoutOrNull

--- src/practice/17practice.kt
+++ src/practice/17practice.kt
@@ -1,2 +1,32 @@
 package practice
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+
+fun simple_1(): Flow<Int> = flow{
+    println("Flow started")
+    for(i in 1..3){
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit>{
+    println("Calling simple_1 function")
+    val flow1 = simple_1()
+    println("Calling collect")
+    flow1.collect { val1 -> println(val1) }
+    println("Calling collect again")
+    flow1.collect { val2 -> println(val2) }
+}
+
+
+/*
+Flow is cold : means that the code inside the flow builder does not run
+until the flow is collected.
+
+Flow started line will only print when collect is called, not when object
+is created.
+ */
---
+++ src/practice/18practice.kt
@@ -0,0 +1,25 @@
+package practice
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
+
+fun simple_2(): Flow<Int> = flow {
+    for (i in 1..3) {
+        delay(100)
+        println("Emitting $i")
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    withTimeoutOrNull(250){
+        simple_2().collect { value -> println(value) }
+    }
+    println("Done")
+}
+
+//flow cancellation
