5af279b655a337ce927c994ca3c0908c39f11265
Piyushh10 <shivnanip815@gmail.com>
2025-10-05 11:34:53 +0530

    flows practice

--- src/practice/16practice.kt
+++ src/practice/16practice.kt
@@ -1,2 +1,29 @@
 package practice
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun simple(): Flow<Int> = flow{
+    for(i in 1..3){
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit>{
+    launch{
+        for(k in 1..3){
+            println("I am not blocked $k")
+            delay(100)
+        }
+    }
+    simple().collect { value -> println(value) }
+}
+
+
+/*
+flow uses emit and collect
+ */
