0ce9bd5f2a8c26bcac6e720c0d5b89387e256315
Piyushh10 <shivnanip815@gmail.com>
2025-10-09 19:01:44 +0530

    collect and launchIn

---
+++ src/practice/35practice.kt
@@ -0,0 +1,28 @@
+package practice
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit>{
+    events().onEach{event -> println("Event: $event")}
+        .collect()
+    println("Done!")
+}
+
+fun events() : Flow<Int> = (1..3).asFlow().onEach { delay(100) }
+
+/*
+the launchIn terminal operator comes in handy here. By replacing collect
+with launchIn(this), we can launch a collection of the flow in a
+separate coroutine, so that execution of further code immediately continues
+
+Then the OP will be-
+Done
+Event: 1
+Event: 2
+Event: 3
+ */
