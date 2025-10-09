48b3af395d2cc06b375beb3ae067885d0210f123
Piyushh10 <shivnanip815@gmail.com>
2025-10-09 15:25:14 +0530

    flattening

--- src/practice/29practice.kt
+++ src/practice/29practice.kt
@@ -2,16 +2,16 @@ package practice
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.zip
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking<Unit>{
     val nums29 = (1..3).asFlow().onEach{ delay(300) }
     val strs29 = flowOf("one", "two", "three").onEach{ delay(400) }
     val startTime29 = System.currentTimeMillis()
-    nums29.zip(strs29){a,b-> "$a -> $b"}
+    nums29.combine(strs29){a,b-> "$a -> $b"}
         .collect { val29 ->
             println("$val29 at ${System.currentTimeMillis()-startTime29} ms from start")
         }
---
+++ src/practice/30practice.kt
@@ -0,0 +1,32 @@
+package practice
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun main() = runBlocking<Unit>{
+    val startTime30 = System.currentTimeMillis()
+    (1..3).asFlow().onEach{delay(100)}
+        .flatMapConcat{requestFlow(it)}
+        .collect { val30 ->
+            println("$val30 at ${System.currentTimeMillis()-startTime30} ms from start")
+        }
+}
+
+fun requestFlow(i: Int): Flow<String> = flow {
+    emit("$i: First")
+    delay(500) // wait 500 ms
+    emit("$i: Second")
+}
+
+
+/*
+Processes each inner flow one after the other,
+in order. Waits for one to finish before starting the next.
+ */
