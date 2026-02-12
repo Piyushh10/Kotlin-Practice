8ea2200b49374c1748d167a20f64140fc9714d9c
Piyushh10 <shivnanip815@gmail.com>
2026-02-12 11:33:00 +0530

    revising kotlin until i get a job

--- src/practice/53practice.kt
+++ src/practice/53practice.kt
@@ -1,4 +1,24 @@
 package practice
 
-class `53practice` {
+fun main(){
+    var list = (1..20).toList()
+    println(list)
+    list = list.filter {
+        it%2==0
+    }
+    println(list)
+
+    list = list.customFilter { it % 2 == 0 }
+    println(list)
 }
+
+fun List<Int>.customFilter(filterFunction: (Int)->(Boolean)) : List<Int>{
+    var list1=mutableListOf<Int>()
+    for(item in this){
+        if(filterFunction(item)){
+            list1.add(item)
+        }
+    }
+    return list1
+}
+m
