f4399249bc0f04442957d8478fa41cc1c3ab4c84
Piyushh10 <shivnanip815@gmail.com>
2026-02-16 19:17:42 +0530

    Practising kotlin till i get a job

--- src/practice/55practice.kt
+++ src/practice/55practice.kt
@@ -16,6 +16,13 @@ fun main(){
     val p4=p2.copy(email = "shivnanip815@gmail.com")
 
     println(DateUtil.format(ZonedDateTime.now()))
+
+    val response = HttpStatus.NOT_FOUND
+    println(response.toResponseString())
+
+    HttpStatus.entries.forEach{
+        println(it.name)
+    }
 }
 
 class SessionStorage(
@@ -49,3 +56,87 @@ object DateUtil{
         return "..."
     }
 }
+
+data object DateUtil2{
+    fun format(dateTime : ZonedDateTime) : String{
+        return "..."
+    }
+}
+
+//same diff as a normal class and a data class, but it is a singleton,
+//so it can only have one instance in the entire program
+
+
+enum class Colors{
+    RED, GREEN, BLUE
+}
+
+//you can also give properties and methods to enum classes, but they dont
+// have special functions like a data class, so they are not used to hold
+// data, but to represent a fixed set of constants
+
+enum class HttpStatus(val code : Int, val message : String){
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found");
+
+    fun toResponseString() : String{
+        return "Error: $code, Message: $message"
+    }
+}
+
+
+sealed class NetworkResult{
+    data class Success(val data : String) : NetworkResult()
+    data class Error(val errorMessage : String) : NetworkResult()
+    data object Loading : NetworkResult()
+}
+
+/*
+sealed classes can have a constructor, but if it doesnt require one,
+then we can use sealed interface instead.
+ */
+
+sealed interface NetworkResult2{
+    data class Success(val data : String) : NetworkResult()
+    data class Error(val errorMessage : String) : NetworkResult()
+    data object Loading : NetworkResult()
+}
+
+abstract class Sensor{
+    abstract val name : String
+    abstract fun startListening(onNewValue : (Float) -> Unit)
+}
+//you already know about abstract classes, same as Java
+
+@JvmInline
+value class Email(val email : String){
+    init{
+        if(!email.contains("@")){
+            throw IllegalArgumentException("Invalid email address")
+        }
+    }
+}
+//it will be treated as a String at runtime, but it provides type
+// safety at compile time, so you cant accidentally pass a normal
+// string where an Email is expected, and it also allows you to add
+// validation logic in the init block to ensure that only valid email
+// addresses are created.
+// So it provides the benefits of a normal class, but without the
+// overhead of creating an object in memory, since it will be treated
+// as a String at runtime.
+
+
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.CONSTRUCTOR
+)
+annotation class DeprecatedClass
+
+//eg
+@DeprecatedClass
+class xyz @DeprecatedClass constructor(){}
+
+//inner class is class inside a class, where you can access everything
+// of the parent class, which will not work if "inner" keyword is not
+// used.
---
+++ src/practice/56Practice.kt
@@ -0,0 +1,18 @@
+package practice
+
+/*
+Scope functions-
+Let
+Also
+Apply
+Run
+With
+ */
+
+fun main(){
+    var number : Int? = null
+    if(number != null){
+        val num2 = number+1
+    }
+
+}
